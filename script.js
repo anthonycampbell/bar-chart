e8ee95a21e923ddada966e02e37c6ae947e09b4b
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-22 02:26:38 -0700

    Created drawBarChart function stub, and call it in html file for the demo.

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-function callAlert(){
-  alert("javascript file has loaded");
+function drawBarChart(data, options, element) {
+  let barChart = $('<div class="bar-chart"></div>');
+  for (let i = 0; i < data.length; i++) {
+    barChart.append('<div class="bar"></div>');
+  }
+  element.append(barChart);
 }
