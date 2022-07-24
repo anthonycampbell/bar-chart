2074027a169b33d34870cc85743686cc619a5877
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-23 20:56:55 -0700

    tidy up demo and separate making bars function from making chart function

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
+function main() {
+  let data = [100, 2, 3, 4];
+  let options = {};
+  let demo = $("#demo");
+  drawBarChart(data, options, demo);
+}
+
 function drawBarChart(data, options, element) {
   let barChart = $('<div class="bar-chart"></div>');
-  for (let i = 0; i < data.length; i++) {
-    barChart.append('<div class="bar"></div>');
-  }
+  makeBars(data, options, barChart);
   element.append(barChart);
+}
+
+function makeBars(data, options, barChart){
+  let bars = [];
+  let bar;
+  for (let i = 0; i < data.length; i++) {
+    bar = $('<div class="bar"></div>');
+    bar.height(data[i]);
+    bars.push(bar);
+  }
+  for (bar of bars) {
+    barChart.append(bar);
+  }
 }
