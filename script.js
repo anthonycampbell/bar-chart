ece9a5ca3f1f1aa8eaac42089d00ac7b763be018
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-25 01:12:19 -0700

    Begun implementing options

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-function makeBars(data, options, barChart){
+function setCSS(bars, property, value) {
+  if (property === 'background-color') {
+    if (!validateColor(value)) return;
+  }
+  for (let i = 0; i < bars.length; i++) {
+    bars[i].element.css(property, value);
+  }
+}
+function applyBarOptions(bars, options) {
+  for (let o in options) {
+    switch (o) {
+      case 'valuePosition':
+        if (options[o].toLowerCase() === 'top') {
+          setCSS(bars, 'align-items', 'flex-start');
+        } else if (options[o].toLowerCase() === 'centre') {
+          setCSS(bars, 'align-items', 'center');
+        } else if (options[o].toLowerCase() === 'bottom') {
+          setCSS(bars, 'align-items', 'flex-end');
+        } else {
+          console.log('Invalid input. Option valuePosition mus be one of top, bottom, or centre');
+        }
+        break;
+    }
+  }
+}
+function makeBars(data, options, barChart) {
   let bars = [];
   let bar;
   let maxHeight = 0;
   for (let i = 0; i < data.length; i++) {
     bar = $('<span class="bar"></span>');
-    bars.push({element: bar, height: data[i]});
-    if (data[i] > maxHeight){
+    bars.push({ element: bar, height: data[i] });
+    if (data[i] > maxHeight) {
       maxHeight = data[i];
     }
   }
+  applyBarOptions(bars, options);
   for (bar of bars) {
     bar.element.text(bar.height)
-    bar.element.height((bar.height/maxHeight)*250);
+    bar.element.height((bar.height / maxHeight) * 250);
     barChart.append(bar.element);
   }
 }
@@ -21,7 +47,7 @@ function drawBarChart(data, options, element) {
   element.append(barChart);
 }
 function main() {
-  let data = [100, 250,500, 100,400,400,400,500,600,400,500,600,400];
+  let data = [100, 250, 500, 100, 400, 400, 400, 500, 600, 400, 500, 600, 400];
   let options = {};
   let demo = $("#demo");
   drawBarChart(data, options, demo);
