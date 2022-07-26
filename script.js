5ea1a5d272b2b2a0ec83570f017dc4bd8c0fbcc4
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-26 02:43:01 -0700

    Added ticks to graph and made them customizeable.

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
-function validateColor(c) {
+const validateColor = function(c) {
   let s = new Option().style;
   s.color = c;
-  console.log()
   return s.color === c;
-}
-function setCSS(bars, property, value) {
+};
+const setCSS = function(bars, property, value) {
   if (property === 'background-color') {
     if (!validateColor(value)) return;
   }
   for (let i = 0; i < bars.length; i++) {
     bars[i].element.css(property, value);
   }
-}
-function applyBarOptions(bars, options) {
+};
+const applyBarOptions = function(bars, options) {
   for (let o in options) {
     switch (o) {
-      case 'valuePosition':
-        if (options[o].toLowerCase() === 'top') {
-          setCSS(bars, 'align-items', 'flex-start');
-        } else if (options[o].toLowerCase() === 'centre') {
-          setCSS(bars, 'align-items', 'center');
-        } else if (options[o].toLowerCase() === 'bottom') {
-          setCSS(bars, 'align-items', 'flex-end');
-        } else {
-          console.log('Invalid input. Option valuePosition mus be one of top, bottom, or centre');
-        }
-        break;
-      case 'barSpacing':
-        if (typeof options[o] === 'number') {
-          setCSS(bars, 'margin-left', options[o] / 2);
-          setCSS(bars, 'margin-right', options[o] / 2);
-        }
-        break;
-      case 'barColour':
-        setCSS(bars, 'background-color', options[o]);
-        break;
-      /*case 'barWidth':
-        if (typeof options[o] === 'number'){
-          setCSS(bars, 'padding-left', options[o]/2);
-          setCSS(bars, 'padding-right', options[o]/2);
-        }
-        break;*/
+    case 'valuePosition':
+      if (options[o].toLowerCase() === 'top') {
+        setCSS(bars, 'align-items', 'flex-start');
+      } else if (options[o].toLowerCase() === 'centre') {
+        setCSS(bars, 'align-items', 'center');
+      } else if (options[o].toLowerCase() === 'bottom') {
+        setCSS(bars, 'align-items', 'flex-end');
+      } else {
+        console.log('Invalid input. Option valuePosition mus be one of top, bottom, or centre');
+      }
+      break;
+    case 'barSpacing':
+      if (typeof options[o] === 'number') {
+        setCSS(bars, 'margin-left', options[o] / 2);
+        setCSS(bars, 'margin-right', options[o] / 2);
+      }
+      break;
+    case 'barColour':
+      setCSS(bars, 'background-color', options[o]);
+      break;
+    /*case 'barWidth':
+      if (typeof options[o] === 'number'){
+        setCSS(bars, 'padding-left', options[o]/2);
+        setCSS(bars, 'padding-right', options[o]/2);
+      }
+      break;*/
     }
   }
-}
-function makeBars(data, options, chart) {
+};
+const makeBars = function(data, options, chart) {
   let bars = [];
   let bar;
   let maxHeight = 0;
@@ -58,18 +57,35 @@ function makeBars(data, options, chart) {
   }
   applyBarOptions(bars, options);
   for (bar of bars) {
-    bar.element.text(bar.height)
+    bar.element.text(bar.height);
     bar.element.height((bar.height / maxHeight) * 250);
     barWindow.append(bar.element);
   }
   chart.append(barWindow);
   return maxHeight;
-}
-function makeTicks(maxHeight, chart, options){
-  let ticks = $('<div class="ticks"></div>')
+};
+const makeTicks = function(maxHeight, chart, options) {
+  let ticks = $('<div class="ticks"></div>');
+  if (!options.tickUnits) options.tickUnits = maxHeight / 10;
+  if (typeof options.tickUnits === 'number') {
+    if (options.tickUnits < maxHeight) {
+      if (maxHeight / options.tickUnits > 250) {
+        options.tickUnits = maxHeight / 10;
+        console.log('That is too many ticks!');
+      }
+      let divOffset = 1;
+      for (let i = options.tickUnits; i <= maxHeight; i += options.tickUnits) {
+        let tick = $('<div class="tick"></div>');
+        let h = 250 - i / maxHeight * 250 - divOffset;
+        tick.css('top', h);
+        ticks.append(tick);
+        divOffset++;
+      }
+    }
+  }
   chart.prepend(ticks);
-}
-function makeLabels(data, chart, options){
+};
+const makeLabels = function(data, chart, options) {
   let label;
   let labels = $('<div class="labels"></div>');
   for (let i = 0; i < data.length; i++) {
@@ -77,8 +93,8 @@ function makeLabels(data, chart, options){
     labels.append(label);
   }
   chart.append(labels);
-}
-function drawBarChart(data, options, element) {
+};
+const drawBarChart = function(data, element, options = {}) {
   let barChart = $('<div class="bar-chart"></div>');
   let labels = $('<div class="labels"></div>');
   let maxHeight = makeBars(data, options, barChart);
@@ -86,17 +102,23 @@ function drawBarChart(data, options, element) {
   makeLabels(data, barChart, options);
   element.append(barChart);
 }
-function main() {
+
+const editTitle = function() {
+  return false;
+};
+const main = function() {
+  // let data = [{ height: 100, label: 'apples' },
+  // { height: 250, label: 'oranges' },
+  // { height: 500, label: 'grapes' },
+  // { height: 100, label: 'pears' },
+  // { height: 400, label: 'mangos' }];
   let data = [100, 250, 500, 100, 400];
   let options = {
     valuePosition: 'top',
     //barSpacing: 100,
+    tickUnits: 20,
     barColour: 'rgb(0, 150, 0)'
   };
   let demo = $("#demo");
-  drawBarChart(data, options, demo);
-  data = [4, 2, 7, 4];
-  options = {};
-  demo = $("#demo1");
-  drawBarChart(data, options, demo);
-}
+  drawBarChart(data, demo, options);
+};
