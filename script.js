21cbc56b2fa3315f697559a1e7f20b2642c1db4a
Anthony Campbell <anthony-campbell@hotmail.com>
2022-07-31 03:54:32 -0700

    validate data passed in by user for stacked bar graphs and create the bars based on the return value while maintaining options

--- script.js
+++ script.js
@@ -45,19 +45,41 @@ const applyBarOptions = function(bars, labels, opts) {
     }
   }
 };
-const validData = function(e) {
-  if (typeof e === 'object' &&
-  typeof e.height === 'number' &&
-  !isNaN(e.height)) {
-    if (e.label && typeof e.label !== 'string') {
-      e.label = '';
-      console.log('label must be string');
+const validData = function(elements) {
+  let bar = {};
+  let chunks = [];
+  let chunkObj = {};
+  let err = 'data must include array of number, array of objects ' +
+  'with height and string or array of objects with ' +
+  'array of objects with height and color and then a label';
+  if (typeof elements === 'object' && !Array.isArray(elements)){
+    for (let e in elements){
+      if (e === 'height' && Array.isArray(elements[e])){
+        for (let chunk of elements[e]){
+          if (typeof chunk === 'object'){
+            for (chunkData in chunk){
+              if (chunkData === 'chunkH' && !isNaN(chunk[chunkData])){
+                chunkObj['chunkH'] = chunk[chunkData];
+              } else if (chunkData === 'color' && validateColor(chunk[chunkData])){
+                chunkObj['color'] = chunk[chunkData];
+              }
+            }
+            chunks.push(chunkObj);
+            chunkObj = {};
+          }
+        }
+        bar['height'] = chunks;
+      } else if (e === 'height' && typeof elements[e] === 'number' && !isNaN(elements[e])){
+        bar['height'] = [elements[e]];
+      } else if (e === 'label' && typeof elements[e] === 'string'){
+        bar['label'] = elements[e];
+      }
     }
-    return e;
-  } else if (typeof e === 'number' && !isNaN(e)) {
-    return {height: e, label: ''};
+    return bar;
+  } else if (typeof elements === 'number' && !isNaN(elements)) {
+    return {height: elements, label: ''};
   } else {
-    console.log('data must array of numbers or objects with height');
+    console.log(err);
     return {height: 0, label: ''};
   }
 };
@@ -69,24 +91,38 @@ const makeBars = function(data, options, chart) {
   let maxHeight = 0;
   let d;
   let columns = [];
+  let chunks = [];
   let barWindow = $('<div class="bar-window"></div>');
-  for (let i = 0; i < data.length; i++) {
+  for (let i in data/*= 0; i < data.length; i++*/) {
     d = validData(data[i]);
-    bar = $('<span class="bar"></span>');
+    let mh = 0;
+    for (let chunk of d.height){
+      bar = $('<span class="bar"></span>');
+      bars.push({ element: bar, height: chunk.chunkH, color: chunk.color });
+      mh += chunk.chunkH;
+    }
+    chunks.push(bars);
+    bars = [];
     label = $(`<span class="label">${d.label}</span>`);
     columns.push($(`<span class="column"></span>`));
     labels.push({element: label, label: d.label});
-    bars.push({ element: bar, height: d.height });
-    if (d.height > maxHeight) maxHeight = d.height;
+    if (mh > maxHeight) maxHeight = mh;
   }
-  applyBarOptions(bars, labels, options);
+  //applyBarOptions(bars, labels, options);
   for (let i = 0; i < columns.length; i++) {
-    bars[i].element.text(bars[i].height);
-    bars[i].element.height((bars[i].height / maxHeight) * 250);
-    columns[i].append(bars[i].element);
+    for (let j = 0; j < chunks[i].length; j++){
+      chunks[i][j].element.text(chunks[i][j].height);
+      chunks[i][j].element.height((chunks[i][j].height / maxHeight) * 250);
+      chunks[i][j].element.css('background-color', chunks[i][j].color);
+      columns[i].append(chunks[i][j].element);
+    }
+    //bars[i].element.text(bars[i].height);
+    //bars[i].element.height((bars[i].height / maxHeight) * 250);
+    // columns[i].append(chunks[i].element);
     columns[i].append(labels[i].element);
     barWindow.append(columns[i]);
-  }chart.append(barWindow);
+  }
+  chart.append(barWindow);
   return maxHeight;
 };
 const checkTickUnits = function(maxHeight, options) {
@@ -133,18 +169,50 @@ const drawBarChart = function(data, element, options = {}) {
   let container = $('<div class="container"></div>');
   let barChart = $('<div class="bar-chart"></div>');
   let maxHeight = makeBars(data, options, barChart);
+  console.log(maxHeight);
   makeTicks(maxHeight, barChart, options);
   makeTitle(container, options);
   container.append(barChart);
   element.append(container);
 };
 const main = function() {
-  let data = [{ height: 100, label: 'apples' },
+  let data = [9, 1, 6, 3, 5];
+  let data0 = [{ height: 100, label: 'apples' },
     { height: 250, label: 'oranges' },
     { height: 500, label: 'grapes' },
     { height: 100, label: 'pears' },
     { height: 400, label: 'mangos' }];
-  //data = [9, 1, 6, 3, 5];
+  let data1 = [
+    { height: [
+      { chunkH: 100, color: 'rgb(150, 150, 0)' },
+      { chunkH: 100, color: 'rgb(0, 150, 150)' },
+      { chunkH: 100, color: 'rgb(150, 0, 150)' } ],
+    label: 'snacks'
+    },
+    { height: [
+      { chunkH: 250, color: 'rgb(150, 150, 0)' },
+      { chunkH: 250, color: 'rgb(0, 150, 150)' },
+      { chunkH: 250, color: 'rgb(150, 0, 150)' } ],
+      label: 'fruits'
+    },
+    { height: [
+      { chunkH: 500, color: 'rgb(150, 150, 0)' },
+      { chunkH: 500, color: 'rgb(0, 150, 150)' },
+      { chunkH: 500, color: 'rgb(150, 0, 150)' } ],
+      label: 'vegetables'
+    },
+    { height: [
+      { chunkH: 100, color: 'rgb(150, 150, 0)' },
+      { chunkH: 100, color: 'rgb(0, 150, 150)' },
+      { chunkH: 100, color: 'rgb(150, 0, 150)' } ],
+      label: 'meats'
+    },
+    { height: [
+      { chunkH: 400, color: 'rgb(150, 150, 0)' },
+      { chunkH: 400, color: 'rgb(0, 150, 150)' },
+      { chunkH: 400, color: 'rgb(150, 0, 150)' } ],
+    label: 'dairy'
+    }];
   let options = {
     valuePosition: 'top',
     barColour: 'rgb(0, 150, 0)',
@@ -157,9 +225,8 @@ const main = function() {
     labelColor: 'blue',
   };
   let demo = $("#demo");
-  drawBarChart(data, demo, options);
+  drawBarChart(data1, demo, options);
 };
 
 // ================================== TODO ============================
-// stacked bars
-//   -color per bar
+// refactor
